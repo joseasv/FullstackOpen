f32c2253df92d45cbb42fd5cfc8d865d66fb2024
José Sánchez <joseasv@outlook.com>
2024-03-29 13:36:15 -0400

    Added part 1 anecdotes exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,7 +14,7 @@ const StatisticsLine = ({text, value, isPercentage}) => {
           {text}
         </td>
         <td>
-          {value} %
+              {value} %
         </td>
       </tr>
     )
@@ -72,7 +72,7 @@ const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [bad, setBad] =  useState(0)
 
   const handleGoodClick = () => setGood(good + 1)
 
