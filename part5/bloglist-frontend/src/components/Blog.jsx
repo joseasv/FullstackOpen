b0c576812ad220040f1aaa8cff45dbe4375c11c4
José Sánchez <joseasv@outlook.com>
2024-05-19 09:36:28 -0400

    Completed part5 bloglist frontend up to 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  //console.log(blog);
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "hide" : "view"}
+      </button>
+      <div style={showWhenVisible}>
+        <div>{blog.url} </div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.user && blog.user.name}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
